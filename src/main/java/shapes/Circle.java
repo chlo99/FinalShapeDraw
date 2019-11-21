1f2c350f008cb45eb984d32f89bb91ad6af13037
chlo99 <chloe.orsini17@imperial.ac.uk>
2019-11-21 11:54:24 +0000

    Initial commit

---
+++ src/main/java/shapes/Circle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+import java.awt.*;
+
+public class Circle extends Shape {
+    private int rad; // Fields
+
+    public Circle(Point initPos, Color col, int radius){
+        super(initPos, col); // The constructor
+        rad=radius; // Initialize fields
+    }
+    @java.lang.Override
+    public void draw(Graphics g) { // A method that draws the object in g
+        g.setColor(col);
+        g.fillOval(pos.x,pos.y,rad, rad);
+    }
+
+}
---
+++ src/main/java/shapes/Square.java
@@ -0,0 +1,11 @@
+package shapes;
+
+import java.awt.*;
+
+public class Square extends Rect {
+
+    public Square(Point initPos, Color col, int width){ // The constructor
+        super (initPos, col, width, width);
+    }
+
+}
